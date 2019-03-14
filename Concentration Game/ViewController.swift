a7ca9a93b74e1f239ed87d19f58da4c46e35f5bc
Mariak Achuoth <achuothmariak@gmail.com>
2019-03-14 16:53:55 -0400

    implement the mvp of concentration game

--- Concentration Game/ViewController.swift
+++ Concentration Game/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    var flipCount: Int = 0{
+        didSet{
+             flipCountLabl.text = "Flips: \(flipCount)"
+        }
     }
-
-
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabl: UILabel!
+    var emojiChoices = ["🍎", "👻", "🍎", "👻","🎃", "🦒", "🦒", "🎃"]
+    
+    @IBAction func flipCardOnTouch(_ sender: UIButton)
+    {
+        //flipCard(withEmoji: "👻", on: sender)
+        if let cardNumber = cardButtons.index(of: sender){
+        flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+        
+        flipCount += 1
+    }
+   
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        }else
+        {
+           button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
